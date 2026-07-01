69e4532f64df2d38ed6e17ed268c04db1b43549e
darklight9911 <muniaedu911@gmail.com>
2026-07-01 23:31:50 +0600

    learned escape

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask import request
+from markupsafe import escape
 
 app = Flask(__name__)
 
@@ -7,7 +9,19 @@ app = Flask(__name__)
 def hello_world():
     return "Hello World"
 
+@app.route("/hello")
+def hello():
+    name = request.args.get("name", "<img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuVSHCLGhllIzFvqt08pmOpX_1bD2-zO12iynALHoDb1qicCYCZErKas8&s\">")
+    return f"Hello, {escape(name)}!"
+    # return f"Hello, {name}!"
 
+@app.route("/welcome/<username>")
+def welcome(username):
+    return f"Welcome, {escape(username)}!"
+
+@app.route("/user/<uuid:user_uuid>")
+def users_uuid(user_uuid):
+    return f"Users uuid : {escape(user_uuid)}"
 
 @app.route("/status")
 def status():
